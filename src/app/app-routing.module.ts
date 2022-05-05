22b1b74ec701bcff5796e432b1fb0e7890fa35ef
pdunbar1999 <peterdunbar1999@gmail.com>
2022-05-05 15:22:23 -0500

    Added login/register to the toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MetrComponent } from '../app/metr/metr.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: MetrComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
